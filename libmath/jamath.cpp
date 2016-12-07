24f7398baa35861805f9a31c6ffb802f4c97f306
joseantoniobou <joseantoniobou@icloud.com>
2016-12-07 20:33:44 +0100

    Creo una libreria simple para poder incluirla en el proyecto del CMake

---
+++ libmath/jamath.cpp
@@ -0,0 +1,7 @@
+#include "jamath.h"
+
+double jamath::mysqrt(double ib_value)
+{
+	return sqrt(ib_value);
+}
+
---
+++ libmath/jamath.h
@@ -0,0 +1,8 @@
+#include <cmath>
+
+class jamath
+{
+public:
+        static double mysqrt(double ib_value);
+};
+
